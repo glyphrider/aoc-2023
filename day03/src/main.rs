aa016c26b0a137f7b10fc7e0e425c2b19fe9e2f8
Brian H. Ward <glyphrider@gmail.com>
2024-05-21 07:47:48 -0400

    clean up

--- day03/src/main.rs
+++ day03/src/main.rs
@@ -1,130 +1,103 @@
+use regex::Regex;
+
 struct Symbol {
     key: char,
     line: usize,
     column: usize,
 }
 
-struct Number {
+struct Part {
     line: usize,
     start: usize,
     end: usize,
-    part: u32,
+    number: u32,
 }
 
 fn main() {
-    let mut numbers: Vec<Number> = vec![];
+    let mut parts: Vec<Part> = vec![];
     let mut symbols: Vec<Symbol> = vec![];
-    create_vectors(include_str!("../input.txt"), &mut numbers, &mut symbols);
-    let result1 = part1(&numbers, &symbols);
-    let result2 = part2(&numbers,&symbols);
+
+    create_vectors(include_str!("../input.txt"), &mut parts, &mut symbols);
+    
+    let result1 = part1(&parts, &symbols);
+    let result2 = part2(&parts, &symbols);
 
     println!("result1 = {}", result1);
     println!("result2 = {}", result2);
 }
 
-fn part1(numbers: &Vec<Number>, symbols: &Vec<Symbol>) -> u32 {
-    return numbers
+fn part1(parts: &Vec<Part>, symbols: &Vec<Symbol>) -> u32 {
+    return parts
         .iter()
         .filter(|number| symbols.iter().any(|symbol| in_aura(number, symbol)))
-        .fold(0, |total, number| return number.part + total);
+        .fold(0, |total, number| return number.number + total);
     // let mut result1 = 0;
 }
 
-fn part2(numbers: &Vec<Number>, symbols: &Vec<Symbol>) -> u32 {
+fn part2(parts: &Vec<Part>, symbols: &Vec<Symbol>) -> u32 {
     return symbols
         .iter()
         .filter(|symbol| symbol.key == '*')
-        .fold(0,|ratios,symbol| {
-            let gears = numbers.iter().filter(|number| in_aura(number,symbol));
+        .fold(0, |ratios, symbol| {
+            let gears = parts.iter().filter(|part| in_aura(part, symbol));
             if gears.clone().count() == 2 {
-                return ratios + numbers.iter().filter(|number| in_aura(number,symbol)).fold(1,|acc,gear| acc * gear.part);
+                return ratios
+                    + parts
+                        .iter()
+                        .filter(|number| in_aura(number, symbol))
+                        .fold(1, |acc, gear| acc * gear.number);
             }
             return ratios;
         });
 }
 
-fn create_vectors(input_data: &str, mut numbers: &mut Vec<Number>, mut symbols: &mut Vec<Symbol>) {
+fn create_vectors(input_data: &str, mut parts: &mut Vec<Part>, mut symbols: &mut Vec<Symbol>) {
     input_data
         .lines()
         .enumerate()
         .for_each(|(line_number, line)| {
-            process_line(line, line_number, &mut numbers, &mut symbols);
+            process_line(line, line_number, &mut parts, &mut symbols);
         });
 }
 
 fn process_line(
     line: &str,
     line_number: usize,
-    numbers: &mut Vec<Number>,
+    numbers: &mut Vec<Part>,
     symbols: &mut Vec<Symbol>,
 ) {
-    // println!("processing => {}",line);
-    let mut reading_digit_string = false;
-    let mut number_start: usize = 0;
-    let mut number_end: usize = 0;
-    for (i, c) in line.chars().enumerate() {
-        number_end = i;
-        if c.is_digit(10) {
-            if !reading_digit_string {
-                reading_digit_string = true;
-                number_start = i;
-            }
-            continue;
-        } else {
-            if reading_digit_string {
-                reading_digit_string = false;
-                let part: u32 = line[number_start..i].parse().unwrap();
-                numbers.insert(
-                    numbers.len(),
-                    Number {
-                        line: line_number + 1,
-                        start: number_start + 1,
-                        end: i + 1,
-                        part: part,
-                    },
-                );
-                // println!("  found number {} on line {} at {}..{}",part,line_number,number_start,i);
-            }
-        }
-        if c == '.' {
-            continue;
-        }
-        // symbol
-        let symbol = Symbol {
-            line: line_number + 1,
-            column: i + 1,
-            key: c,
-        };
-        // println!("  found symbol at ({},{})",symbol.line,symbol.column);
-        symbols.insert(symbols.len(), symbol);
-    }
-    if reading_digit_string {
-        number_end = number_end + 1;
-        let part: u32 = line[number_start..number_end].parse().unwrap();
-        numbers.insert(
-            numbers.len(),
-            Number {
-                line: line_number + 1,
-                start: number_start + 1,
-                end: number_end + 1,
-                part: part,
-            },
-        );
-        // println!("  found number {} on line {} at {}..{}",part,line_number,number_start,i);
-    }
+    process_numbers_on_line(line, line_number, numbers);
+    process_symbols_on_line(line, line_number, symbols);
 }
 
-// fn part_number_value(part: &Number, symbols: &Vec<Symbol>) -> u32 {
-//     for symbol in symbols {
-//         if is_adjacent(part, symbol) {
-//             return part.part;
-//         }
-//     }
-//     // println!("part number {} ({},{}..{}) is invalid",part.part,part.line,part.start,part.end);
-//     return 0;
-// }
+fn process_numbers_on_line(line: &str, line_number: usize, parts: &mut Vec<Part>) {
+    let re = Regex::new(r"\d+").unwrap();
+    re.find_iter(line)
+        .map(|m| Part {
+            line: line_number + 1,
+            start: m.start() + 1,
+            end: m.start() + m.len() + 1,
+            number: m.as_str().parse().unwrap(),
+        })
+        .for_each(|p| {
+            parts.push(p);
+        });
+}
 
-fn in_aura(part: &Number, symbol: &Symbol) -> bool {
+fn process_symbols_on_line(line: &str, line_number: usize, symbols: &mut Vec<Symbol>) {
+    let re = Regex::new(r"[^\.0-9]").unwrap();
+    re.find_iter(line)
+        .map(|m| Symbol {
+            key: m.as_str().chars().next().unwrap(),
+            line: line_number + 1,
+            column: m.start() + 1,
+        })
+        .for_each(|s| {
+            symbols.push(s);
+        });
+}
+
+fn in_aura(part: &Part, symbol: &Symbol) -> bool {
     return symbol.line >= part.line - 1
         && symbol.line <= part.line + 1
         && symbol.column >= part.start - 1
